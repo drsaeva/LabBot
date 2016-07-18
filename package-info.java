2260663c41be021964b31aa898e1337d823ff12a
David R. Saeva <drsaeva@gmail.com>
2016-07-18 15:08:10 -0400

    Create package-info.java

---
+++ package-info.java
@@ -0,0 +1,12 @@
+/**
+ *  This package is designed to automate the entry of paper laboratory 
+ *  reports into the eHARS document system. To this end, Tess4J and 
+ *  Selenium are used for OCR of scanned lab reports and IE browser
+ *  automation, respectively.
+ *  
+ */	
+/**
+ * @author David R. Saeva
+ *
+ */
+package LabBot;
