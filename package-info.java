790dc0436550eeaacfea64dcd9a2ba09a4cac03b
David R. Saeva <drsaeva@gmail.com>
2016-07-22 15:12:54 -0400

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -7,6 +7,12 @@
  */	
 /**
  * @author David R. Saeva
+ * 
+ * This project is licensed under the GNU GENERAL PUBLIC LICENSE, 
+ * Version 3, 29 June 2007. Please see the gpl.txt file included 
+ * in this package, or visit https://www.gnu.org/licenses/gpl.txt, 
+ * for precise terms and conditions for copying, distribution and 
+ * modification of this project.
  *
  */
 package labBot;
