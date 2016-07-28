624a6962aceb5db981a422709091593340462e4d
David R. Saeva <drsaeva@gmail.com>
2016-07-28 12:02:16 -0400

    Create LabDocData.java

---
+++ LabDocData.java
@@ -0,0 +1,84 @@
+package labBot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
+public class LabDocData {
+
+	private String dataSourceType, dataValue, dataFieldHtmlId, dataSection;
+	private final static Map<String, String> dataSource = ImmutableMap.<String,String>builder()
+			.put("reportingCity", "repHlthDeptName")
+			.put("dateReceived", "receiveDt")
+			.put("documentSource", "documentSourceCd")
+			.put("isNewCase", "initinvest")
+			.put("reportMedium", "rptMedium")
+			.put("surveillanceMethod", "survMethod")
+			.put("dateComplete", "completeDt")
+			.put("lastName", "lastName")
+			.put("firstName", "firstName")
+			.put("middleName", "middleName")
+			.put("lastName", "lastName")
+			.put("zipCode", "zipCd")
+			.put("streetAddress", "streetAddress1")
+			.put("streetAddressExtra", "streetAddress2")
+			.put("cityName", "cityName")
+			.put("cntyName", "cntyName")
+			.put("phoneNumber", "phone1")
+			.put("idType", "idCd")
+			.put("idValue", "idValue")
+			.put("birthSex", "birthSex")
+			.put("dob", "dob")
+			.build();
+	
+	public static void createLabDocData(HashMap<String, LabDocData> dataInDoc, String dataSourceType, String dataValue) {
+		dataInDoc.put(dataSourceType, new LabDocData(dataSourceType, dataValue));
+	}
+	
+	public LabDocData(String dataSourceType, String dataValue) {
+		// TODO Auto-generated constructor stub
+		
+		setDataValue(dataValue);
+		setDataSection(dataSection);
+		setDataSourceType(dataSourceType);
+		setDataFieldHtmlId(dataSourceType);
+	}
+	
+	protected void setDataSourceType(String dataSourceType) {
+			this.dataSourceType = dataSourceType;
+	}
+	
+	public String getDataSourceType() {
+		return dataSourceType;
+	}
+	
+	private void setDataValue(String dataValue) {
+		this.dataValue = dataValue;
+	}
+	
+	public String getDataValue() {
+		return dataValue;
+	}
+	
+	private void setDataFieldHtmlId(String dataSourceType) {
+		this.dataFieldHtmlId = getDataFieldHtmlIdFromHash(dataSourceType);
+	}
+	
+	public String getDataFieldHtmlId() {
+		return dataFieldHtmlId;
+	}
+	
+	private String getDataFieldHtmlIdFromHash(String dataSourceType) {
+		return dataSource.get((dataSourceType));
+	}
+	
+	private void setDataSection (String dataSection) {
+		this.dataSection = dataSection;
+	}
+	
+	public String getDataSection () {
+		return dataSection;
+	}
+	
+}
