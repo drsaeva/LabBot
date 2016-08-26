169903e93c6a4d60b9d63f849e41d5664d952133
David R. Saeva <drsaeva@gmail.com>
2016-08-26 10:47:30 -0400

    removed deprecated code, isNew Case
    isNewCase object to be instantiated by FindCase class

--- FormInfo.java
+++ FormInfo.java
@@ -35,39 +35,10 @@ public class FormInfo {
 	}*/
 	
 	public FormInfo(/*String dateComplete*/) {
-		// TODO Examine code - do I need anything else aside from these methods being run?
-		
 		setUnchangingVariables();
 		setDateReceived();
-		setNewCase();
-		//setDateComplete(dateComplete);
-		
-		
 	}
-	
-	private void setNewCase(/*newCaseBool*/) {
-		// TODO code to set new case Y/N answer
-		/*
-		 * Requirements - regex for case name   <--- set via Identification Parsing Object
-		 * 				- search for case name  <--- results likely set via SearchCase Object
-		 * 						* needs - multiple methods of searching (DOB + fullname, DOB + L or F name, SSN, etc)
-		 * 								- search logic: if DOB+FN fails, do alternate search; if all fail, new case (?)
-		 * 				- must produce String from bool value: "Y", "N", or "U" 
-		 */
-		String isNewCase;
-		/* 
-		 * 
-		 * if (newCaseBool) {
-		 * 		isNewCase = "T";
-		 * } else {
-		 * 		isNewCase = "N";
-		 * }
-		 */
-		isNewCase = "N"; //default behavior until search is coded
-		LabDocData.createLabDocData(LabDocScanner.dataInDoc, "isNewCase", isNewCase);
-		//this.isNewCase = isNewCase;
-	}
-	
+
 	private void setDateReceived() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd"); //Always today?
 		Date today = new Date();
@@ -81,40 +52,4 @@ public class FormInfo {
 		LabDocData.createLabDocData(LabDocScanner.dataInDoc, "reportMedium", reportMedium);
 		LabDocData.createLabDocData(LabDocScanner.dataInDoc, "surveillanceMethod", surveillanceMethod);
 	}
-	
-	/*
-	private void setDateComplete(String dateComplete) {
-		this.dateComplete = dateComplete;
-	}
-	
-	public String getNewCase() {
-		return isNewCase;
-	}
-	
-	protected String getDateReceived(LabDocScanner scanner) {
-		return scanner.getDataFromHashMap("dateReceived").getDataValue(); 
-	}
-	
-	protected String getDateComplete() {
-		return dateComplete;
-	}
-	
-	public String getDocumentSource() {
-		return documentSource;
-	}
-	
-	public String getSurveillanceMethod() {
-		return surveillanceMethod;
-	}
-	
-	public String getReportMedium() {
-		return reportMedium;
-	}
-	
-	public String getReportingCity() {
-		return reportingCity;
-	}*/
-	
-	
-	
 }
